82d18e6d3debe111a3818c334d0ef0108c8bb0ad
main <claudia.raphael73@gmail.com>
2025-12-02 20:52:19 -0300

    complete requirements.txt

--- logger.py
+++ logger.py
@@ -1,1 +1,38 @@
 # sistema de logs
+from logging.config import dictConfig
+import logging
+import os
+
+log_path = "log/"
+if not os.path.exists(log_path):
+   os.makedirs(log_path)
+
+dictConfig({
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] %(levelname)-4s %(funcName)s() L%(lineno)-4d %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "default",
+            "filename": "log/app.log",
+            "maxBytes": 10000000,
+            "backupCount": 5,
+        }
+    },
+    "root": {
+        "handlers": ["console", "file"],
+        "level": "INFO",
+    }
+})
+
+logger = logging.getLogger(__name__)
